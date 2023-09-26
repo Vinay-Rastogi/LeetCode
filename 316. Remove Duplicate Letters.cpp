36350830d87cc60ef0f741bcc730eab70ebcf0bc
Vinay-Rastogi <mvinayrastogi@gmail.com>
2023-09-26 07:41:25 +0530

    Remove Duplicate Letters

---
+++ 316. Remove Duplicate Letters.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    string removeDuplicateLetters(string s)
+    {
+
+        int n = s.size();
+        string res = "";
+        unordered_map<char, int> m;
+
+        for (char it : s)
+            m[it]++;
+
+        vector<int> v(26, -1);
+        stack<char> st;
+
+        for (char i : s)
+        {
+
+            m[i]--;
+
+            if (v[i - 'a'] != -1)
+                continue;
+
+            while (!st.empty() && st.top() > i && m[st.top()] > 0)
+            {
+                v[st.top() - 'a'] = -1;
+                st.pop();
+            }
+
+            st.push(i);
+            v[i - 'a'] = 1;
+        }
+
+        while (!st.empty())
+        {
+            res += st.top();
+            st.pop();
+        }
+
+        reverse(res.begin(), res.end());
+
+        return res;
+    }
+};
+
+int main()
+{
+
+    Solution sl;
+    string s = "cbacdcbc";
+
+    cout << sl.removeDuplicateLetters(s);
+
+    return 0;
+}
