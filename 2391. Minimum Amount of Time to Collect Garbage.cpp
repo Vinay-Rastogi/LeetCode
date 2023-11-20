4dea7ffd8b5764fdb96ee3d9f05c3428aabda830
Vinay-Rastogi <mvinayrastogi@gmail.com>
2023-11-20 17:18:44 +0530

    Minimum Amount of Time To Collect Garbage

---
+++ 2391. Minimum Amount of Time to Collect Garbage.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int garbageCollection(vector<string> &garbage, vector<int> &travel)
+    {
+        int n = garbage.size();
+        int lastM = -1, lastP = -1, lastG = -1;
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+
+            if (lastM == -1 && garbage[i].find("M") != string::npos)
+                lastM = i;
+            if (lastP == -1 && garbage[i].find("P") != string::npos)
+                lastP = i;
+            if (lastG == -1 && garbage[i].find("G") != string::npos)
+                lastG = i;
+
+            if (lastM != -1 && lastP != -1 && lastG != -1)
+                break;
+        }
+
+        int ans = 0;
+
+        for (auto g : garbage)
+            ans += g.size();
+
+        if (lastM != -1)
+            ans = accumulate(travel.begin(), travel.begin() + lastM, ans);
+        if (lastP != -1)
+            ans = accumulate(travel.begin(), travel.begin() + lastP, ans);
+        if (lastG != -1)
+            ans = accumulate(travel.begin(), travel.begin() + lastG, ans);
+
+        return ans;
+    }
+};
+
+int main()
+{
+
+    Solution sl;
+    vector<string> garbage = {"G", "P", "GP", "GG"};
+    vector<int> travel = {2, 4, 3};
+
+    cout << sl.garbageCollection(garbage, travel);
+
+    return 0;
+}
