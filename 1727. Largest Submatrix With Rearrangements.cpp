8eeba1233c99aff95cdf8237a1bb621ad33d0c49
Vinay-Rastogi <mvinayrastogi@gmail.com>
2023-11-26 09:50:39 +0530

    Largest Submatrix With Rearrangements

---
+++ 1727. Largest Submatrix With Rearrangements.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int largestSubmatrix(vector<vector<int>> &matrix)
+    {
+        int m = matrix.size(), n = matrix[0].size();
+        int ans = 0;
+        for (int row = 0; row < m; row++)
+        {
+            for (int col = 0; col < n; col++)
+            {
+                if (matrix[row][col] != 0 && row > 0)
+                {
+                    matrix[row][col] += matrix[row - 1][col];
+                }
+            }
+
+            vector<int> currRow = matrix[row];
+            sort(currRow.begin(), currRow.end(), greater<int>());
+
+            for (int i = 0; i < n; i++)
+            {
+                ans = max(ans, currRow[i] * (i + 1));
+            }
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+
+    Solution sl;
+    vector<vector<int>> matrix = {{0, 0, 1}, {1, 1, 1}, {1, 0, 1}};
+
+    cout << sl.largestSubmatrix(matrix);
+
+    return 0;
+}
