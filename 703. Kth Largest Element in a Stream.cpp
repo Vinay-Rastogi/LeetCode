b6c2705901f8f06a53537e9104b90f83db211628
Vinay Rastogi <mvinayrastogi@gmail.com>
2024-08-12 09:19:39 +0530

    Kth Largest Element in a Stream

--- 703. Kth Largest Element in a Stream.cpp
+++ 703. Kth Largest Element in a Stream.cpp
@@ -1,27 +1,40 @@
-class KthLargest
-{
+#include <iostream>
+#include <BITS/stdc++.h>
+
+using namespace std;
+
+class KthLargest {
 public:
     priority_queue<int, vector<int>, greater<int>> pq;
     int sz;
-    KthLargest(int k, vector<int> &nums)
-    {
 
+    KthLargest(int k, vector<int>& nums) {
         sz = k;
-
-        for (int i = 0; i < nums.size(); i++)
-        {
+        for (int i = 0; i < nums.size(); i++) {
             pq.push(nums[i]);
+        }
+        while (pq.size() > sz) {
+            pq.pop();
         }
     }
 
-    int add(int val)
-    {
-
+    int add(int val) {
         pq.push(val);
-        while (pq.size() > sz)
-        {
+        while (pq.size() > sz) {
             pq.pop();
         }
         return pq.top();
     }
 };
+
+int main() {
+    vector<int> nums = {4, 5, 8, 2};
+    KthLargest* obj = new KthLargest(3, nums);
+
+    cout << obj->add(3) << endl; 
+    cout << obj->add(5) << endl; 
+    cout << obj->add(10) << endl; 
+    cout << obj->add(9) << endl; 
+    cout << obj->add(4) << endl; 
+
+}
