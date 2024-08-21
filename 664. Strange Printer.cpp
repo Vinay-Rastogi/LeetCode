5e2288f72e8c2a7a122e9ab339db2a90d9dc7a4c
Vinay Rastogi <mvinayrastogi@gmail.com>
2024-08-21 08:44:24 +0530

    Strange Printer Modified(Added Main)

--- 664. Strange Printer.cpp
+++ 664. Strange Printer.cpp
@@ -1,90 +1,46 @@
-class Solution
-{
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
 public:
     int n;
     vector<vector<int>> dp;
 
-    int f(int i, int j, string &s)
-    {
-
+    int f(int i, int j, string& s){    
         if (dp[i][j] != -1)
             return dp[i][j];
 
-        if (i == j)
+        if (i == j) 
             return dp[i][j] = 1;
 
         int ans;
 
-        if (s[i] == s[j] || s[j - 1] == s[j])
-            ans = f(i, j - 1, s);
-
-        else if (s[i] == s[i + 1])
-            ans = f(i + 1, j, s);
-
-        else
-        {
-
-            ans = f(i, j - 1, s) + 1;
-
-            for (int k = i + 1; k < j; k++)
-            {
+        if (s[i] == s[j] || s[j-1] == s[j])
+            ans = f(i, j-1, s);
+        else if (s[i] == s[i+1])
+            ans = f(i+1, j, s);
+        else {
+            ans = f(i, j-1, s) + 1;
+            for (int k = i + 1; k < j; k++) {
                 if (s[k] == s[j])
-                    ans = min(ans, f(i, k - 1, s) + f(k, j - 1, s));
+                    ans = min(ans, f(i, k-1, s) + f(k, j-1, s));
             }
         }
-
+        
         return dp[i][j] = ans;
     }
 
-    int strangePrinter(string s)
-    {
+    int strangePrinter(string s) {
         n = s.size();
         dp.assign(n, vector<int>(n, -1));
-        return f(0, n - 1, s);
+        return f(0, n-1, s);
     }
 };
-class Solution
-{
-public:
-    int n;
-    vector<vector<int>> dp;
 
-    int f(int i, int j, string &s)
-    {
-
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        if (i == j)
-            return dp[i][j] = 1;
-
-        int ans;
-
-        if (s[i] == s[j] || s[j - 1] == s[j])
-            ans = f(i, j - 1, s);
-
-        else if (s[i] == s[i + 1])
-            ans = f(i + 1, j, s);
-
-        else
-        {
-
-            ans = f(i, j - 1, s) + 1;
-
-            for (int k = i + 1; k < j; k++)
-            {
-                if (s[k] == s[j])
-                    ans = min(ans, f(i, k - 1, s) + f(k, j - 1, s));
-            }
-        }
-
-        return dp[i][j] = ans;
-    }
-
-    int strangePrinter(string s)
-    {
-        n = s.size();
-        dp.assign(n, vector<int>(n, -1));
-        return f(0, n - 1, s);
-    }
-};
+int main() {
+    Solution sol;
+    string s = "aaabbb";
+    int result = sol.strangePrinter(s);
+    cout << "Minimum number of turns the printer needs: " << result << endl;
+    return 0;
+}
