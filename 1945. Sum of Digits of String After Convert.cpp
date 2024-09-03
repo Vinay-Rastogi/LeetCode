6b77325df73269b944f259670396dc3373a456ad
Vinay Rastogi <mvinayrastogi@gmail.com>
2024-09-03 10:48:48 +0530

    Sum of Digits of String After Convert

---
+++ 1945. Sum of Digits of String After Convert.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int sumDigits(int num)
+    {
+        int sum = 0;
+        while (num)
+        {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
+    }
+
+    int getLucky(string s, int k)
+    {
+
+        int sum = 0;
+        for (char &ch : s)
+        {
+            int val = ch - 'a' + 1;
+            sum += val < 10 ? val : (val % 10 + val / 10);
+        }
+
+        k -= 1;
+        while (k-- && sum >= 10)
+            sum = sumDigits(sum);
+
+        return sum;
+    }
+};
+
+int main()
+{
+    Solution sl;
+    string s = "leetcode";
+    int k = 2;
+
+    return 0;
+}
